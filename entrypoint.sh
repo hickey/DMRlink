6b0cdadde1da5f446ee580f1f6cef8d7b0824557
Gerard Hickey <hickey@kinetic-compute.com>
2019-12-02 00:51:36 -0800

    Docker collateral

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cmd=$1
+shift
+
+if [[ "$cmd" != "" ]]; then
+    # if first arg is an executable then run it
+    if [[ -x $cmd ]]; then
+        exec $cmd $@
+    else
+        # search $PATH for $cmd
+        for dir in ${PATH//:/ }; do
+            if [[ -x "$dir/$cmd" ]]; then
+                exec "$dir/$cmd" $@
+            fi
+        done
+    fi
+fi
+
+config=''
+if [[ -r /etc/dmrlink.cfg ]]; then
+    config="--config /etc/dmrlink.cfg"
+fi
+
+exec python ./dmrlink.py $config $cmd $@
